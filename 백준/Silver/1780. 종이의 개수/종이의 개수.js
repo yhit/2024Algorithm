41890dec93cfe5b69cc42812cb853f6c081fdd02
sheep_hy <122270969+yhit@users.noreply.github.com>
2024-01-14 22:01:02 +0900

    [Silver II] Title: 종이의 개수, Time: 712 ms, Memory: 99040 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.js
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.js
@@ -1,30 +1,50 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const n = +input[0];
-const paper = input.slice(1).map(v => v.split(" ").map(vv => +vv));
+const fs = require('fs');
+let [N, ...paper] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const countPaper = n => {
-    const count = [0, 0, 0];
-    const recursion = (n, x, y) => {
-        const num = paper[y][x];
-        let numCount = 0;
-        for (let i=0; i<n; i++) {
-            for (let j=0; j<n; j++) {
-                if (paper[y+j][x+i] === num) numCount++;
-                else break;
-            }
-        }
-        if (numCount === n*n) count[num+1]++;
-        else {
-            n /= 3;
-            for (let i=0; i<3; i++) {
-                for (let j=0; j<3; j++) {
-                    recursion(n, x+i*n, y+j*n);
-                }
-            }
-        }
+N = N * 1;
+paper = paper.map(line => line.split(' '));
+
+// 계수 변수 선언
+let minusOne = 0;
+let zero = 0;
+let one = 0;
+
+// 함수 실행 및 결과갑 출력
+divideConquer(0, 0, N);
+const answer = minusOne + '\n' + zero + '\n' + one;
+console.log(answer);
+
+// 분할 정복 함수
+function divideConquer(column, row, length) {
+  if (checkBox(column, row, length)) {
+    const initialValue = paper[column][row];
+    if (initialValue === '-1') {
+      minusOne++;
+    } else if (initialValue === '0') {
+      zero++;
+    } else {
+      one++;
     }
-    recursion(n, 0, 0);
-    console.log(count.join("\n"));
-};
+  } else {
+    const dividedLength = length / 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        divideConquer(column + dividedLength * i, row + dividedLength * j, dividedLength);
+      }
+    }
+  }
+}
 
-countPaper(n);
+// 분면 확인 함수
+function checkBox(column, row, length) {
+  const initialValue = paper[column][row];
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (paper[column + i][row + j] !== initialValue) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
