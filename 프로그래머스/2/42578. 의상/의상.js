61be986cce19d66f5fd5e38734e8c751f42bbcbd
sheep_hy <122270969+yhit@users.noreply.github.com>
2024-01-23 10:36:24 +0900

    [level 2] Title: 의상, Time: 3.05 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,21 @@
+function solution(clothes) {
+    let box = new Map();
+    let answer = 1;
+
+    for(let i =0 ; i < clothes.length; i++){
+        if(box.get(clothes[i][1])){
+            box.set(clothes[i][1],box.get(clothes[i][1])+1)
+        }else{
+            box.set(clothes[i][1],1);
+        }
+    }
+
+    for(let value of box.values()){
+        
+        answer *= (value + 1);
+        console.log(value)
+        console.log(answer)
+    }
+
+    return answer - 1;
+}
